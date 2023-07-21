0402d6dacc9d1d4d3a040a349f65a645afb9f726
toleubekbogenbai <toleubek.bogenbai@gmail.com>
2023-07-21 11:32:25 +0600

    first

---
+++ src/test/java/kz/todo/todobackend/TodobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.todo.todobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodobackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
